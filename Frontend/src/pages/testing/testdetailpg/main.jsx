73e071b37dc08ad5b0ffbd914d9235038174fbba
JishnuTP <tpjishnu5@gmail.com>
2024-12-02 11:11:09 +0530

    exam-spinner

--- Frontend/src/pages/testing/testdetailpg/main.jsx
+++ Frontend/src/pages/testing/testdetailpg/main.jsx
@@ -50,7 +50,10 @@ const TestdetailpgMain = () => {
   }, [testId]);
 
   if (loading) {
-    return <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 border-blue-500 rounded-full border-t-transparent"></div>; // Show loading state while fetching data
+    return <div className="flex items-center justify-center min-h-screen">
+    <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 border-blue-500 rounded-full border-t-transparent"></div>
+  </div>
+   // Show loading state while fetching data
   }
 
   if (error) {
